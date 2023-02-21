e8d514dac5c344d0a7973ba04ee9a964088f3ed0
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-21 17:50:14 -0500

    Changing city format

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,7 +10,7 @@ const Weather = () => {
     const data = useWeather();
     if(data.current) return(
         <>
-        <h1 className="font-thin text-6xl w-full text-center mx-auto mt-4 ">{`${data.location.name}`}</h1>
+        <h1 className="font-thin text-4xl w-full text-center mx-auto mt-4 ">{`${data.location.name}, ${data.location.country}`}</h1>
         <h2 className="font-thin text-7xl text-center ml-4">{data.current.temp_c}ºC</h2>
         <div className=" font-thin flex flex-col text-xl w-full justify-center items-center gap-4">
         <img className="opacity-60 p-2   rounded-lg blur-[0.5px]" src={data.current.condition.icon}/>
