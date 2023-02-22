2c3a1bc7f3b674ae6dabf445bd5a9e3c61894539
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-22 18:42:44 -0500

    Changing styles for layout and cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ const Card = (props) => {
     if(props?.type == 'forecast'){
       let hour = props.time?.split(' ');
       return(
-        <div className="flex flex-col m-2 bg-gray-100/20 shadow-sm rounded-lg shadow-black items-center justify-evenly">
+        <div className="flex flex-col m-2  shadow-[1px_4px_5px_3px] shadow-gray-900/50 rounded-lg  items-center justify-evenly">
           <p className='font-thin text-center text-sm'>{hour[1]}</p>
           <img src={props.condition?.icon} alt="" className=" h-1/2" />
             <p className="text-center font-thin text-xs w-[80px]">{`${props?.temp}ºC`}</p>
@@ -12,7 +12,7 @@ const Card = (props) => {
   }
     else{
       return(
-        <div className="flex flex-col m-2 bg-gray-100/20 shadow-sm rounded-lg shadow-black items-center justify-evenly">
+        <div className="flex flex-col m-2  shadow-[1px_4px_5px_3px] shadow-gray-900/50 rounded-lg items-center justify-evenly">
           <p className='font-thin text-center text-sm'>Now</p>
           <img src={props.data?.condition.icon} alt="" className=" h-1/2" />
             <p className="text-center font-thin text-xs w-[80px]">{`${props.data?.temp_c}ºC`}</p>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,13 +3,13 @@ import { Menu } from "./Menu";
 import { Header } from "./Header";
 import { useWeather } from "../hooks/useWeather";
 const Layout = (props) => {
-    const {current} = useWeather();
-    console.log(current?.is_day)
+    const {current, forecast} = useWeather();
+    console.log(forecast)
     return(
     <>
         <Header />
-        <main className={`w-full min-h-screen ${(current?.is_day) ? "bg-hero":"bg-dark-hero"} bg-center grid place-items-center`}>  
-      <div className="bg-white/40 blur-[0.5px] container relative w-1/3 min-w-[350px] my-12 flex flex-col shadow-md shadow-black gap-4 rounded-lg">
+        <main className={`w-full min-h-screen ${(!current?.is_day) ? "bg-hero":"bg-dark-hero"} bg-center grid place-items-center`}>  
+      <div className="blur-[0.4px] container relative w-1/3 min-w-[350px] my-12 flex flex-col shadow-[1px_4px_5px_3px] shadow-gray-900/50 gap-4 rounded-lg">
         {props.children}
       </div>
       </main>
