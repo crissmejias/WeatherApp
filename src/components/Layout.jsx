b09051b2282976821a5e6d9efce8ad8e51030032
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-22 19:08:59 -0500

    Adding API link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,10 +8,11 @@ const Layout = (props) => {
     return(
     <>
         <Header />
-        <main className={`w-full min-h-screen ${(!current?.is_day) ? "bg-hero":"bg-dark-hero"} bg-center grid place-items-center`}>  
-      <div className="blur-[0.4px] container relative w-1/3 min-w-[350px] my-12 flex flex-col shadow-[1px_4px_5px_3px] shadow-gray-900/50 gap-4 rounded-lg">
+        <main className={`w-full min-h-screen ${(!current?.is_day) ? "bg-hero":"bg-dark-hero"} bg-center grid grid-rows-[1fr_200px] place-items-center`}>  
+      <div className="blur-[0.4px py-4 container relative w-1/3 min-w-[350px] flex flex-col shadow-[1px_4px_5px_3px] place-self-center sef-center shadow-gray-900/50 gap-4 rounded-lg">
         {props.children}
       </div>
+      <a target="_blank" href="https://www.weatherapi.com/" title="Free Weather API" className=" text-center p-2 blur-[0.5px] shadow-[1px_2px_5px_1px] shadow-gray-900/50 rounded-lg font-thin self-start mb-8 place-self-center mb-4 text-[16px]">Powered by <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"/></a>
       </main>
     </>
     )
