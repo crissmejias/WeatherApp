01ff7875ba9ce689128527cfd4c12338c16f1734
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-24 16:04:51 -0500

    First commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()]})
+export default defineConfig({ plugins: [react()], base:'/WeatherApp/'})
 
 
